40bce33bf7708d3a9dd1fdfe3382178f6a047204
gurjarsumit366-svg <gurjarsumit366@gmail.com>
2026-04-10 08:40:23 +0530

    this is UC4 solution

---
+++ UC4/OOPSBannerApp.java
@@ -0,0 +1,27 @@
+/**
+ * OOPSBannerApp
+ * UC4 - Print OOPS banner using Array and Loop
+ *
+ * @author Abhishek
+ * @version 4.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+
+            String.join("", " *****   ", " *****   ", " *****   ", " ***** "),
+            String.join("", "*     * ", "*     * ", "*     * ", "*     *"),
+            String.join("", "*     * ", "*     * ", "*     * ", "*      "),
+            String.join("", "*     * ", "*****   ", " *****  ", " ***** "),
+            String.join("", "*     * ", "*       ", "*            ", "* "),
+            String.join("", "*     * ", "*       ", "*      ", "*     *"),
+            String.join("", " *****  ", "*       ", " *****  ", " ***** ")
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
