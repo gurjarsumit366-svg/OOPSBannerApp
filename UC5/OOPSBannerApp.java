f041baaded86f0d4704ed585262222137d54398e
gurjarsumit366-svg <gurjarsumit366@gmail.com>
2026-04-10 08:43:04 +0530

    this is UC5 solution

---
+++ UC5/OOPSBannerApp.java
@@ -0,0 +1,26 @@
+/**
+ * OOPSBannerApp
+ * UC5 - Print OOPS banner using compact array initialization
+ *
+ * @author Abhishek
+ * @version 5.0
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+            String.join("", " *****   ", " *****   ", " *****   ", " ***** "),
+            String.join("", "*     * ", "*     * ", "*     * ", "*     *"),
+            String.join("", "*     * ", "*     * ", "*     * ", "*      "),
+            String.join("", "*     * ", "*****   ", " *****  ", " ***** "),
+            String.join("", "*     * ", "*       ", "*            ", "* "),
+            String.join("", "*     * ", "*       ", "*      ", "*     *"),
+            String.join("", " *****  ", "*       ", " *****  ", " ***** ")
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
